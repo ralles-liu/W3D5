841fac057cf441cfcebc5e8d170117eaad597651
ralles <ralles.liu@gmail.com>
2020-11-16 14:12:13 -0500

    finishes building the supercomptuer

--- tic_tac_toe/lib/super_computer_player.rb
+++ tic_tac_toe/lib/super_computer_player.rb
@@ -1,6 +1,35 @@
 require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
+
+  def move(game, mark)
+    my_node = TicTacToeNode.new(game.board, mark)
+    #next_move_marker is the computer
+    #my_move.prev = humans move except the first time 
+
+    #my_node.children creates all the child nodes that stem from potential "mark" moves
+    my_node.children.each do |child|
+      # in child, human is the next player, and prev_move is what the computer just moved
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    # the computer tests its moves, and if that move results in a winning_node? it returns the move it just tested
+    # each child is a move that the computer is testing 
+    # if that child state is winning, that means the computer wants to reach that state
+    # what was child's previous move that allowed it to get there
+
+    my_node.children.each do |child|
+      if !child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    raise "there are no non-losing nodes"
+
+
+  end
+
   
 end
 
